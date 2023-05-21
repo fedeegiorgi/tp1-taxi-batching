62dee2e3cf5c429d3ea26d84e0bec18d0eb3ad3c
fedeegiorgi <fgiorgi@mail.utdt.edu>
2023-05-21 19:12:49 -0300

    comments greedy solver

--- code/greedy_solver.cpp
+++ code/greedy_solver.cpp
@@ -17,21 +17,10 @@ void GreedySolver::setInstance(TaxiAssignmentInstance &instance) {
 }
 
 void GreedySolver::solve() {
-    // prueba: entendiendo como funciona la calse TaxiAssignmentSolution
-
-    // crea objeto solucion con n (cant. pax y taxis)
-
-    // tenemos para usar:
-    // _solution.getN()
-    // _solution.assign(i-esimo taxi, j-esimo pax) 
-    // _solution.isTaxiAssigned(i-esimo taxi)
-    // _solution.isPaxAssigned(j-esimo pax)
-
-    // _instance.dist[i-esimo taxi][j-esimo pax] 
-
     // objetivo: completar los vectores _taxi_assignment y _pax_assignment de _solution tal que
     // _solution.getAssignedPax(i-esimo taxi) y _solution.getAssignedTaxi(j-esimo pax) den solución para todo i y j
     
+    // Inicializamos timer.
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
 
@@ -52,9 +41,10 @@ void GreedySolver::solve() {
         this->_objective_value += this->_instance.dist[min_index][j];
     }
 
+    // Frenamos timer.
     end = std::chrono::system_clock::now();
     std::chrono::duration<double> duration = end - start;
-
+    
     this->_solution_time = duration.count();
 }
 
